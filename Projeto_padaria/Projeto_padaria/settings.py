009677bb8bac36178d697085341846330bd81e69
xKuth <luisyeager20@gmail.com>
2024-11-23 21:33:57 -0300

    modify templates and ignores

--- Projeto_padaria/Projeto_padaria/settings.py
+++ Projeto_padaria/Projeto_padaria/settings.py
@@ -7,9 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-_aq^7*x*%*lld*dvaisvhdt#f+h40smb7dcymq!)+idq&nti2%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
