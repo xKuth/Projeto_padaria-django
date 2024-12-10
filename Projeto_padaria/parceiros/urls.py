05db1ffeb2149df5cfeb60d71a4457ac123aefb5
xKuth <luisyeager20@gmail.com>
2024-12-10 18:28:08 -0300

    add load statics, and add css on hmtl5 on frist page in app001 the project

--- Projeto_padaria/parceiros/urls.py
+++ Projeto_padaria/parceiros/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from Projeto_padaria import settings
 
 urlpatterns = [
     path('', views.PaginaP, name='pagina001'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
