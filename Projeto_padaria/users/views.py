fa34da55a63c90eef487738c0b3a34d88aa3fcd6
xKuth <luisyeager20@gmail.com>
2025-01-19 10:52:04 -0300

    create views user authenticad

--- Projeto_padaria/users/views.py
+++ Projeto_padaria/users/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import user
 
 # Create your views here.
+
 def logonP(request):
+    if request.User
+    
+    username = request.POST['username']
+    password = request.POST['password']
+    authentic = authenticate(request, username=username, password=password)
+
+    if authentic is not None:
+        login(request, authentic)
     return render(request, 'html/login.html')
 
 def registerP(request):
