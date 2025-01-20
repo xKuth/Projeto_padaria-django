5109e068b0a6c6fd5ec3b1c257cf2bf8b3ac5678
xKuth <luisyeager20@gmail.com>
2025-01-20 13:27:21 -0300

    add button logout for users login

--- Projeto_padaria/users/models.py
+++ Projeto_padaria/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.forms import ModelForm
+from django import forms
 
 # Create your models here.
+'''class RegistrationForm(forms.ModelForm):
+    password = models.
+    Class Meta:'''
