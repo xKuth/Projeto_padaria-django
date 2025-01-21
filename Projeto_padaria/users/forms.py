eb22a97678ac1e1d50def1de52c9ddf9417d17cc
xKuth <luisyeager20@gmail.com>
2025-01-21 15:25:15 -0300

    create a new form on document form.py , one form for db login

--- Projeto_padaria/users/forms.py
+++ Projeto_padaria/users/forms.py
@@ -1,2 +1,12 @@
-from django.db import models
-from django import forms
+from django import forms as formulario
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, forms
+
+
+class LoginForms(formulario.Form):
+
+    form_user = formulario.CharField(min_legth=5, max_length=20, 
+                                     widget=formulario.TextInput(attrs=('class: ')))
+    form_password = formulario.CharField(min_length=8, max_length=64, 
+                                         widget=(formulario.PasswordInput(attrs=['class: '])))
--- Projeto_padaria/users/models.py
+++ Projeto_padaria/users/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-from django.forms import ModelForm
-from django import forms
-
-# Create your models here.
-class RegistrationForm(models.Model):
-    user = models.CharField(max_length=20)
-    password = models.CharField(max_length=64)
-    
