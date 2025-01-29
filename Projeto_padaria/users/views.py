25e7f444c6803b1259e87359f15307f9077180cf
xKuth <luisyeager20@gmail.com>
2025-01-29 10:56:23 -0300

    add create form on db on views users

--- Projeto_padaria/users/views.py
+++ Projeto_padaria/users/views.py
@@ -22,5 +22,18 @@ def logonP(request):
 
 def registerP(request):
     form = RegisterForm()
+    if request == 'POST':
+        form = RegisterForm(request.POST)
+        if form is not None:
+            if form.is_valid():
+                user_form = form.cleaned_data('form_user')
+                password_form = form.cleaned_data('form_pasword')
+                password_form2 = form.cleaned_data('form_password2')
+                if password_form == password_form2:
+                    form.save()
+                    new_user = authenticate(request, username='user_form', password='password_form')
+                    login(request, new_user)
+            
+
     context = {'form': form}
     return render(request, 'html/register.html', context)
