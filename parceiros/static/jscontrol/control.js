b305f4a119c4779f192a8e0151338590b5816c48
Luis  da Silva <156721982+xKuth@users.noreply.github.com>
2025-06-21 10:03:22 -0300

    add overlayer for external elements and ajust main base.html

---
+++ parceiros/static/jscontrol/control.js
@@ -0,0 +1,8 @@
+elements_li = document.getElementsByClassName();
+const text_external_li = document.createElement('p');
+text_external_li.textContent = '`${}`';
+
+
+buttonexternal(){
+    elemento = document.getElementById('azull');
+};
